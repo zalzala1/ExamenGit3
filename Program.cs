ecbeb93bda60bc51fee37d8543d95a15d75e05c4
zalzala1 <129684437+zalzala1@users.noreply.github.com>
2023-04-16 06:20:24 +0200

    Update Program.cs
    ajout de le class client et modification de la fonction main

--- Program.cs
+++ Program.cs
@@ -3,8 +3,28 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { Hotel hotel = new Hotel();
+
+            Client client1 = new Client("Doe", "John", "john.doe@example.com", "1234-5678-9101-1121", 101);
+            hotel.AjouterClient(client1);
+
+            Client client2 = new Client("Smith", "Jane", "jane.smith@example.com", "4321-8765-1098-7654", 102);
+            hotel.AjouterClient(client2);
+
+            Console.WriteLine("Clients dans l'hôtel :");
+            foreach (Client client in hotel.Clients)
+            {
+                Console.WriteLine($"Nom: {client.Nom}, Prénom: {client.Prenom}, Courriel: {client.Courriel}, Carte de crédit: {client.CarteCredit}, Numéro de chambre: {client.NumeroChambre}");
+            }
             Console.WriteLine("Hello, World!");
+        } 
+        public Client(string nom, string prenom, string courriel, string carteCredit, int numeroChambre)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Courriel = courriel;
+            CarteCredit = carteCredit;
+            NumeroChambre = numeroChambre;
         }
     }
 }
